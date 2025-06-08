d79314e8f3363e7e6a6b9aa415baa49cd04d6983
Varun Kakkar <kakkar.varun67@gmail.com>
2025-06-08 18:23:01 -0400

    Reorg Files and added initial load balancer
    Moved files into logical structure.
    
    Added the initial config for a load balancer
    
    Author: Varun Kakkar <kakkar.varun67@gmail.com>

---
+++ loadbalancer.tf
@@ -0,0 +1,44 @@
+# Load Balancer
+
+#aws_lb
+
+resource "aws_lb" "ngnix_lb" {
+  name = "prog8830-lb-tf"
+  internal = false
+  load_balancer_type = "application"
+
+  security_groups  = [aws_security_group.load_balancer_security_group.id]
+  subnets = [aws_subnet.public_subnet.id]
+  tags = {
+    Name = "PROG8830_LB"
+  }
+
+}
+
+# aws_lb_listener
+resource "aws_lb_listener" "name" {
+  load_balancer_arn = aws_lb.ngnix_lb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.ngnix_target_group.arn
+  }
+}
+
+# aws_lb_target_group
+resource "aws_lb_target_group" "name" {
+name = "ngnix-target-group"
+port = 80
+protocol = "HTTP"
+vpc_id =  aws_vpc.app.id
+  
+}
+
+# aws_lb_group_attachments
+resource "aws_lb_target_group_attachment" "name" {
+  target_group_arn = aws_lb_target_group.ngnix_target_group.arn
+  target_id = aws_instance.nginx1.id
+  port = 80
+}
