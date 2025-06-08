f55b35352ddb886978bdfb9c4bea87617dfa00f4
Varun Kakkar <kakkar.varun67@gmail.com>
2025-06-08 19:33:25 -0400

    Corrected Load balancer
    Corrected Load Balancer traget group attachments, load balancer listener
    corrected the name from ngnix to "nginx"
    
    Author: Varun Kakkar <kakkar.varun67@gmail.com>

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -2,12 +2,12 @@
 
 #aws_lb
 
-resource "aws_lb" "ngnix_lb" {
-  name = "prog8830-lb-tf"
-  internal = false
+resource "aws_lb" "nginx_lb" {
+  name               = "prog8830-lb-tf"
+  internal           = false
   load_balancer_type = "application"
 
-  security_groups  = [aws_security_group.load_balancer_security_group.id]
+  security_groups = [aws_security_group.load_balancer_security_group.id]
   subnets         = [aws_subnet.public_subnet.id, aws_subnet.public_subnet_2.id]
   tags = {
     Name = "PROG8830_LB"
@@ -16,31 +16,31 @@ resource "aws_lb" "ngnix_lb" {
 }
 
 # aws_lb_listener
-resource "aws_lb_listener" "name" {
-  load_balancer_arn = aws_lb.ngnix_lb.arn
-  port = 80
-  protocol = "HTTP"
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.nginx_lb.arn
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.ngnix_target_group.arn
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_target_group.arn
   }
 }
 
-# aws_lb_target_group
-resource "aws_lb_target_group" "name" {
-name = "ngnix-target-group"
-port = 80
-protocol = "HTTP"
-vpc_id =  aws_vpc.app.id
-  
+# # aws_lb_target_group_attachment 
+resource "aws_lb_target_group" "nginx_target_group" {
+  name     = "ngnix-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app.id
+
 }
 
 # aws_lb_group_attachments
-resource "aws_lb_target_group_attachment" "name" {
-  target_group_arn = aws_lb_target_group.ngnix_target_group.arn
-  target_id = aws_instance.nginx1.id
-  port = 80
+resource "aws_lb_target_group_attachment" "nginx1" {
+  target_group_arn = aws_lb_target_group.nginx_target_group.arn
+  target_id        = aws_instance.nginx1.id
+  port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "nginx2" {
