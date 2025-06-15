34cbcee74656a0228d0a2352a60fc0a768fc6ff5
Varun Kakkar <kakkar.varun67@gmail.com>
2025-06-15 19:27:16 -0400

    W6: Modified the instances to use a single source
    Migrated the user data shell script into it's own shell file called
    initial-server-setup.sh.
    
    This file should be a single place where we can reference the initial
    server configuration for each instance.
    
    Author: Varun Kakkar <kakkar.varun67@gmail.com>

--- instances.tf
+++ instances.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-
 # INSTANCES
 resource "aws_instance" "nginx1" {
   ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
@@ -11,13 +5,7 @@ resource "aws_instance" "nginx1" {
 
   subnet_id       = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.public_security_group.id]
-  user_data       = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
+  user_data       = file("./initial-server-setup.sh")
 
 }
 
@@ -31,13 +19,6 @@ resource "aws_instance" "nginx2" {
   subnet_id       = aws_subnet.public_subnet_2.id
   security_groups = [aws_security_group.public_security_group.id]
 
-  user_data = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
+  user_data = file("./initial-server-setup.sh")
 
 }
-
