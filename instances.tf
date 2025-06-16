69b3e3042d7b11581dfbd90df673713a66d7d862
Varun Kakkar <kakkar.varun67@gmail.com>
2025-06-15 20:16:19 -0400

    W6: Added input and output variables
    Input and output variables introduced to optimize the functionality of
    our terraform config.
    
    Author: Varun Kakkar <kakkar.varun67@gmail.com>

--- instances.tf
+++ instances.tf
@@ -1,13 +1,22 @@
 # INSTANCES
 resource "aws_instance" "nginx1" {
   ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type = "t3.micro"
+  
+  instance_type = var.instance_type
+
+  cpu_options {
+    core_count = var.core_count
+  }
 iam_instance_profile = aws_iam_instance_profile.nginx_profile.name
 
 
   subnet_id       = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.public_security_group.id]
   user_data       = file("./initial-server-setup.sh")
+  tags = {
+    "Name": "nginx2"
+  }
+
 
 }
 
@@ -15,7 +24,7 @@ iam_instance_profile = aws_iam_instance_profile.nginx_profile.name
 
 resource "aws_instance" "nginx2" {
   ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   iam_instance_profile = aws_iam_instance_profile.nginx_profile.name
 
@@ -24,6 +33,11 @@ resource "aws_instance" "nginx2" {
   security_groups = [aws_security_group.public_security_group.id]
 
   user_data = file("./initial-server-setup.sh")
+
+  tags = {
+    "Name": "nginx2"
+  }
+
 
   }
 resource "aws_iam_instance_profile" "nginx_profile" {
